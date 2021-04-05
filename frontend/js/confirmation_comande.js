21a6086b3cf63b3cb72279959aaa36b9b0754d96
inabernic <bernic.in@gmail.com>
2021-04-05 19:31:09 +0200

    POST request pour la confirmation de commande

---
+++ frontend/js/confirmation_comande.js
@@ -0,0 +1,35 @@
+async function registerOrder() {
+    let contact = {
+        lastName: document.querySelector('#validationCustom01').innerText,
+        firstName: document.querySelector('#validationCustom02').innerText,
+        address: document.querySelector('#validationCustom06').innerText,
+        city: document.querySelector('#validationCustom06').innerText,
+        email: document.querySelector('#validationCustomUsername03').innerText,
+    }
+
+    let productIds = []
+    let products = JSON.parse(localStorage.getItem('productsKey'));
+    for (let product of products) {
+        productIds.push(product._id)
+    }
+
+    let body = {
+        contact: contact,
+        products: productIds
+    }
+
+    let json = JSON.stringify(body)
+    console.log(json)
+    myHeaders = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    let confirmationOrder = await fetch("http://localhost:3000/api/cameras/order", {
+        method: "POST",
+        headers: myHeaders,
+        body: json
+    }).then(response => response.json())
+    window.localStorage.clear();
+    alert(`Votre numero de commande est:`+ confirmationOrder.orderId)
+    document.location.href = `index.html`
+}
