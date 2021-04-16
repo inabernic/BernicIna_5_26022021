51926cd0093336f45eb1a9d0ff31c16226458596
inabernic <bernic.in@gmail.com>
2021-04-16 15:03:34 +0200

    mise en page

--- frontend/js/displayProduits.js
+++ frontend/js/displayProduits.js
@@ -1,29 +1,30 @@
-import {
-    getProduits
-} from './getProduits.js'
+import { getProduits } from "./getProduits.js";
 
 export const displayProduits = async () => {
-    let produits = []
-    const appHtml = document.querySelector('#app')
-    const templateElem = document.getElementById('template')
+  let produits = [];
+  const appHtml = document.querySelector("#app");
+  const templateElem = document.getElementById("template");
 
+  try {
+    produits = await getProduits();
 
-    try {
-        produits = await getProduits()
+    for (let produit of produits) {
+      const clone = document.importNode(templateElem.content, true);
+      clone.getElementById("idProduit").textContent = produit._id;
+      clone.getElementById("imgProduit").src = produit.imageUrl;
+      clone.getElementById(
+        "nameProduit"
+      ).textContent = `Modèle : ${produit.name}`;
+      clone.getElementById("priceProduit").textContent = `Prix : ${
+        produit.price / 100
+      } €`;
+      clone.getElementById("bouton_info").onclick = function () {
+        document.location.href = `produit.html?id=` + produit._id;
+      };
 
-        for (let produit of produits) {
-            const clone = document.importNode(templateElem.content, true)
-            clone.getElementById("idProduit").textContent = produit._id
-            clone.getElementById("imgProduit").src = produit.imageUrl
-            clone.getElementById("nameProduit").textContent = `Modèle : ${produit.name}` 
-            clone.getElementById("priceProduit").textContent = `Prix : ${produit.price /100} €` 
-            clone.getElementById("bouton_info").onclick = function () {
-                document.location.href = `produit.html?id=` + produit._id
-            }
-
-            appHtml.appendChild(clone)
-        }
-    } catch (error) {
-        console.error(error.message)
+      appHtml.appendChild(clone);
     }
-}
+  } catch (error) {
+    console.error(error.message);
+  }
+};
--- frontend/js/getProduits.js
+++ frontend/js/getProduits.js
@@ -1,3 +1,4 @@
-export const getProduits = async () => (await fetch('http://localhost:3000/api/cameras')
-    .then(response => response.json())
-    .catch(error => alert(error)))
+export const getProduits = async () =>
+  await fetch("http://localhost:3000/api/cameras")
+    .then((response) => response.json())
+    .catch((error) => alert(error));
